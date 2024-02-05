4cd48474347dfea89e01bd4a178eb6555fbbe31a
DZcrafting <david@24net.dk>
2024-02-05 20:10:30 +0100

    Initial commit

---
+++ src/main/java/net/dazeful/vanilljabiomes/VanilljabiomesClient.java
@@ -0,0 +1,10 @@
+package net.dazeful.vanilljabiomes;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VanilljabiomesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
